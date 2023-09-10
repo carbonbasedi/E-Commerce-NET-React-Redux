7a1f8c201d845099666477e8d3298cadba858313
carbonbasedi <carbonbasedai@gmail.com>
2023-09-11 03:20:44 +0400

    Apply loading fix

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -59,7 +59,7 @@ export default function ProductCard({ product }: Props) {
             onClick={() =>
               dispatch(addBasketItemAsync({ productId: product.id }))
             }
-            loading={status.includes("pendingAddItem" + product.id)}
+            loading={status === "pendingAddItem" + product.id}
             size="small"
           >
             Add to Cart
