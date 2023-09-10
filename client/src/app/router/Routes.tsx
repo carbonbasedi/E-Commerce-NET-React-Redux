7617815845562ffd106e0729d98f524d10104ad9
carbonbasedi <carbonbasedai@gmail.com>
2023-09-10 22:06:42 +0400

    Github action added

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -1,6 +1,5 @@
 import { Navigate, createBrowserRouter } from "react-router-dom";
 import App from "../layout/App";
-import HomePage from "../../features/home/HomePage";
 import Catalog from "../../features/catalog/Catalog";
 import ProductDetails from "../../features/catalog/ProductDetails";
 import AboutPage from "../../features/about/AboutPage";
@@ -31,10 +30,6 @@ export const router = createBrowserRouter([
             element: <Orders />,
           },
         ],
-      },
-      {
-        path: "",
-        element: <HomePage />,
       },
       {
         path: "catalog",
